7521aa1a253e6f7c7569271ed467240305ea5e68
Brad <brad@brad.tw>
2018-04-15 10:06:18 +0800

    v4

--- app/src/main/java/tw/org/iii/mycontenttest/MainActivity.java
+++ app/src/main/java/tw/org/iii/mycontenttest/MainActivity.java
@@ -1,15 +1,26 @@
 package tw.org.iii.mycontenttest;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.CallLog;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private ContentResolver contentResolver;
@@ -19,6 +30,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CALL_LOG)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS,
+                            Manifest.permission.READ_CALL_LOG},
+                    0);
+        }else{
+            init();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            init();
+        }
+    }
+
+    private void init(){
         contentResolver = getContentResolver();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
@@ -28,9 +62,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }
-
-
     }
+
 
     // read settings
     public void test1(View view) {
@@ -80,4 +113,69 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void test3(View view) {
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor =
+                contentResolver.query(uri,
+                        null,null,
+                        null,null);
+
+        String[] fields = cursor.getColumnNames();
+        for (String field : fields){
+            Log.v("brad", field);
+        }
+
+        Log.v("brad", "---------------------------");
+        Log.v("brad", ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+//        while (cursor.moveToNext()){
+//            String id = cursor.getString(
+//                    cursor.getColumnIndex(
+//                            ContactsContract.CommonDataKinds.Phone._ID));
+//            String name = cursor.getString(
+//                    cursor.getColumnIndex(
+//                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+//            String number = cursor.getString(
+//                    cursor.getColumnIndex(
+//                            ContactsContract.CommonDataKinds.Phone.NUMBER));
+//
+//            Log.v("brad", name + ":" + number);
+//        }
+
+
+    }
+
+
+    public void test4(View view) {
+        Uri uri = CallLog.Calls.CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri,
+                null,null,
+                null,null);
+
+        while (cursor.moveToNext()){
+            String name = cursor.getString(
+                    cursor.getColumnIndex(
+                    CallLog.Calls.CACHED_NAME));
+            String number = cursor.getString(
+                    cursor.getColumnIndex(
+                            CallLog.Calls.NUMBER));
+            String type = cursor.getString(
+                    cursor.getColumnIndex(
+                            CallLog.Calls.TYPE));
+
+            SimpleDateFormat sdf = new SimpleDateFormat(
+                    "yyyy-MM-dd HH:mm:ss");
+            Date date = new Date(
+                    cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DATE)));
+            String calldate = sdf.format(date);
+
+            int duration = cursor.getInt(cursor.getColumnIndex(CallLog.Calls.DURATION));
+
+            Log.v("brad", calldate+ ":" + name + ":" + number + ":" + duration);
+
+        }
+
+        // MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+    }
 }
